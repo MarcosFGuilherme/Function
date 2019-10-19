6586aa837d28b73f6cf5ccfe56aa1d9c9f798152
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-19 16:26:59 -0300

    Update - Reference method with non-statistical method.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,10 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
 
 public class Program {
 
@@ -21,7 +19,8 @@ public class Program {
 		showProduct(list);
 		
 		//List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
-		List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+		//List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
 		showNames(names);
 		
 	}
